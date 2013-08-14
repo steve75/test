ff48bf470532ef5eb8e80fa3497655335f67f7cd
steve75 <stefancarlsson1@gmail.com>
2013-08-14 17:32:55 +0200

    Commit 2.

---
+++ java/src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Stefan
+ * Date: 2013-08-14
+ * Time: 17:31
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+
+
+
+}
